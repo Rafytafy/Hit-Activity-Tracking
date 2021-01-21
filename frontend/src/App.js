77d904e633348d70cba1520e202c6df435ac0c20
Danielson9712 <55254935+Danielson9712@users.noreply.github.com>
2021-01-21 14:58:25 -0500

    2nd commit
    created starter code, inital component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+//components
+import register from "./components/register";
 
 function App() {
   return (
     <div className="App">
-      
+      <register> </register>
     </div>
   );
 }
---
+++ frontend/src/components/register.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+const register = () => {
+
+    return (
+      <div className = "register"> </div>  
+    );
+}
+
+export default register;
