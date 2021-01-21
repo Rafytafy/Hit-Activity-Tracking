4eb64dab4421f9948db5f4f684c9b783f12baae3
Rafael Montes <rafaelmontes1997@gmail.com>
2021-01-21 18:42:21 -0500

    Add firebase config for server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 const express    = require('express'),
       bodyParser = require('body-parser'),
+      admin      = require('firebase-admin'),
+      serviceAccount = require('serviceAccountKey.json'),
       app        = express();
+
+
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+databaseURL: "https://hit-activity-tracking-default-rtdb.firebaseio.com"
+});
 
 
 //Bodyparser 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+const isAuthenticated = (req, res, next) => {
+
+}
+
+app.post('/register', isAuthenticated, (req, res)=>{
+
+})
 
 const port = process.env.PORT || 5000;
 
