3e38c4758f22b04ee204eff6dc4d4ff1b3ea9d2d
Rafael Montes <rafaelmontes1997@gmail.com>
2021-01-21 19:32:59 -0500

    Add middleware.js and move isAuthenticated function to file

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,24 @@
-const express    = require('express'),
-      bodyParser = require('body-parser'),
-      cors       = require('cors'),
-      admin      = require('firebase-admin'),
+const express        = require('express'),
+      bodyParser     = require('body-parser'),
+      cors           = require('cors'),
+      admin          = require('firebase-admin'),
       serviceAccount = require('./serviceAccountKey.json'),
-      app        = express();
+      app            = express();
 
+const { isAuthenticated } = require('./middleware')
 
 admin.initializeApp({
 credential: admin.credential.cert(serviceAccount),
 databaseURL: "https://hit-activity-tracking-default-rtdb.firebaseio.com"
 });
 
-
 //Bodyparser and cors
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const isAuthenticated = (req, res, next) => {
-    let tokenId = req.body.tokenId
-    console.log(tokenId)
-    admin
-        .auth()
-        .verifyIdToken(tokenId)
-        .then((decodedToken) => {
-            const uid = decodedToken.uid;
-            req.body.uid = uid;
-
-            next();
-        })
-        .catch((error) => {
-            res.send(`There was an error in authentication. Error: ${error}`)
-        });
-}
-
+//routes 
 app.post('/register', isAuthenticated, (req, res)=>{
-    console.log(req.body.uid)
     res.send(req.body.uid)
 })
 
