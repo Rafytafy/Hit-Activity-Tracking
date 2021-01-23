ded8fa86928abd0d829041c469797871f47841fe
Danielson9712 <55254935+Danielson9712@users.noreply.github.com>
2021-01-23 01:57:32 -0500

    4th commit
    added simple form, state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 //components
 import Register from "./components/Register";
 
 function App() {
+  
   return (
     <div className="App">
       <Register />
--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,9 +1,67 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 
-const Register = () => {
+import { Form, FormGroup, Label, Input, FormFeedback, FormText, Button, Row } from 'reactstrap';
 
-    return (
-      <div className = "Register"> </div>  
+const Register = (props) => {
+//States
+  const [] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  return (
+    <div className="Register">
+      
+        <Form> 
+        <FormGroup>
+          <Row>
+            <Input onChange={(firstName) => setFirstName(firstName)}
+              type="text"
+              name="firstName"
+              placeholder="First Name"> </Input>
+          </Row>
+          <Row>
+            <Input onChange={(lastName) => setLastName(lastName)}
+              type="text"
+              name="lastName"
+              placeholder="Last Name"> </Input>
+          </Row>
+          <Row>
+            <Input onChange={(email) => setEmail(email)}
+              type="Email"
+              name="email"
+              placeholder="Email"> </Input>
+          </Row>
+          <Row> 
+            <Input onChange={(password) => setPassword(password)}
+              type="password"
+              name="Password"
+              placeholder="Password"> </Input>
+          </Row>
+          <Row> 
+            <Input onChange={(setHeight) => setHeight(height)}
+              type="text"
+              name="Height"
+              placeholder="Height"> </Input>
+        </Row>
+          <Row> 
+            <Input onChange={(setWeight) => setWeight(weight)}
+              type="text"
+              name="Weight"
+              placeholder="Weight"> </Input>
+        </Row>
+      </FormGroup>
+      </Form>
+        <Button color="secondary" size="lg">Register</Button>
+    </div>
+     
+    
+      
+    
+     
+  
     );
 }
 
