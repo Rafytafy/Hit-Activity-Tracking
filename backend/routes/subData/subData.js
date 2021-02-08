7ee98a45f1ea5c170b0dcea04a669e189c02d340
ParfDesai <55568333+ParfDesai@users.noreply.github.com>
2021-02-08 11:08:48 -0500

    uid thingy

--- backend/routes/subData/subData.js
+++ backend/routes/subData/subData.js
@@ -6,7 +6,7 @@ const { isAuthenticated } = require('../../middleware')
 const Subscriber = require('../../models/Subscriber');
 
 router.get('/getProfileData', isAuthenticated, (req, res) => {
-    Subscriber.findOne({uid : req.body.tokenId},  (err,user) =>{
+    Subscriber.findOne({uid : req.body.uid},  (err,user) =>{
         if(err) {console.log( err)}
         else{console.log(user)}
 
